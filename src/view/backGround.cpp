97cb305f81d74235f23e25514b9b0bc17ddd5c4d
sir-asin <ybnyady0@gmail.com>
2024-07-08 17:39:26 +0330

    background added

--- src/view/backGround.cpp
+++ src/view/backGround.cpp
@@ -1,5 +1,46 @@
-//
-// Created by mobile kavosh on 7/8/2024.
-//
-
 #include "backGround.h"
+#include <QGraphicsScene>
+#include <iostream>
+
+BackGround::BackGround(int sceneHeight, int sceneWidth, QGraphicsItem *parent) : QGraphicsPixmapItem(parent),
+                                                                                 Decorator(sceneWidth, sceneHeight) {
+    setFlags(GraphicsItemFlag::ItemIsFocusable);
+
+
+    QPixmap pixmap(":/images/background");
+    pixmap = pixmap.scaled(pixmap.width(), sceneHeight, Qt::KeepAspectRatioByExpanding);
+    scaledImage1 = new QPixmap(pixmap);
+    pixmap = pixmap.copy(0, 0, pixmap.width(), pixmap.height());
+    setPixmap(pixmap);
+}
+
+void BackGround::moveRight() {
+
+    current_x = (current_x + 5) % (scaledImage1->width() - width - 1000);
+
+
+    QPixmap cropped = scaledImage1->copy(current_x, 0, width, height);
+
+    setPixmap(cropped);
+};
+
+
+void BackGround::moveLeft() {
+
+    if (current_x > 0)
+        current_x = (current_x - 5) % (scaledImage1->width() - width - 1000);
+
+    if (current_x <= 0) {
+        current_x = 11643 - width;
+    }
+
+    QPixmap cropped = scaledImage1->copy(current_x, 0, width, height);
+
+    setPixmap(cropped);
+
+}
+
+
+BackGround::~BackGround() {
+    delete scaledImage1;
+}
--- src/view/backGround.h
+++ src/view/backGround.h
@@ -1,10 +1,26 @@
+#ifndef AMIN_DOPINGI_BACKGROUND_H
+#define AMIN_DOPINGI_BACKGROUND_H
 
-#ifndef GIT_AMIN_BACKGROUND_H
-#define GIT_AMIN_BACKGROUND_H
+#include <QGraphicsPixmapItem>
+#include "decorator.h"
+
+class BackGround : public QObject, public QGraphicsPixmapItem, public Decorator {
 
 
-class BackGround {
+public:
+    BackGround(int, int, QGraphicsItem *parent = nullptr);
 
+    ~BackGround();
+
+    int current_x{0};
+
+    QPixmap *scaledImage1;
+
+public slots:
+
+    virtual void moveRight();
+
+    virtual void moveLeft();
 };
 
 
