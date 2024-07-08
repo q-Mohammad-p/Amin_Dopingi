769d14b66fa1869c986c4b5bd415cedf0d6fe993
sir-asin <ybnyady0@gmail.com>
2024-07-08 19:21:16 +0330

    flag added

--- src/view/flag.cpp
+++ src/view/flag.cpp
@@ -1,3 +1,48 @@
 
-
 #include "flag.h"
+
+flag::flag(Position pos) : BodyObject(pos){
+    QPixmap pixmap(":/images/RedFlag");
+    pixmap = pixmap.scaled(100, 100, Qt::KeepAspectRatioByExpanding);
+    setPixmap(pixmap);
+    setPos(position.x, position.y);
+
+    X = position.x;
+    Y = position.y;
+
+}
+
+void flag::moveFlagRight() {
+    setPos(x() - 5, y());
+}
+
+void flag::moveFlagLeft() {
+    setPos(x() + 5, y());
+}
+
+void flag::changeFlag(int x, int y) {
+    if (checkPoint) {
+        return;
+    }
+    QPixmap pixmap(":/images/BlueFlag");
+    pixmap = pixmap.scaled(100, 100, Qt::KeepAspectRatioByExpanding);
+    setPixmap(pixmap);
+    setPos(x,y);
+    checkPoint = true;
+}
+
+void flag::restart() {
+    setPos(800, Y);
+}
+
+void flag::Reset(){
+    setPos(X,Y);
+}
+
+
+void flag::makeRed(){
+    QPixmap pixmap(":/images/RedFlag");
+    pixmap = pixmap.scaled(100, 100, Qt::KeepAspectRatioByExpanding);
+    setPixmap(pixmap);
+    setPos(X, Y);
+}
--- src/view/flag.h
+++ src/view/flag.h
@@ -1,9 +1,35 @@
+#ifndef AMIN_DOPINGI_FLAG_H
+#define AMIN_DOPINGI_FLAG_H
 
-#ifndef GIT_AMIN_FLAG_H
-#define GIT_AMIN_FLAG_H
 
+#include <QGraphicsPixmapItem>
+#include "bodyObject.h"
 
-class flag {
+class flag : public QObject, public QGraphicsPixmapItem, public BodyObject {
+Q_OBJECT
+
+    Q_PROPERTY(qreal place READ x WRITE setX)
+public:
+    flag(Position);
+
+    void moveFlagRight();
+
+    void moveFlagLeft();
+
+    void changeFlag(int, int);
+
+    bool checkPoint{false};
+
+    void restart();
+
+    void Reset();
+
+    void makeRed();
+
+    int X;
+
+    int Y;
+private:
 
 };
 
