151320a1299b5bc37e9a7ddcc658fec92ae2f4a9
sir-asin <ybnyady0@gmail.com>
2024-07-08 17:42:52 +0330

    hookihooki added

--- src/view/hookihooki.cpp
+++ src/view/hookihooki.cpp
@@ -1,3 +1,64 @@
-
-
 #include "hookihooki.h"
+#include <QPixmap>
+#include <QTimer>
+#include <QPropertyAnimation>
+
+
+hookihooki::hookihooki(int x, int y) {
+    setFlags(GraphicsItemFlag::ItemIsFocusable);
+
+    QPixmap pixmap(":/images/hookihooki");
+    pixmap = pixmap.scaled(60, 60, Qt::KeepAspectRatioByExpanding);
+    setPixmap(pixmap);
+    setPos(x, y);
+
+    X = x;
+    Y = y;
+
+    Right = new QTimer(this);
+    Right->setInterval(100);
+    connect(Right,&QTimer::timeout,this, &hookihooki::runRight);
+    Right->start();
+
+
+    Left = new QTimer(this);
+    Left->setInterval(100);
+    connect(Left,&QTimer::timeout,this, &hookihooki::runLeft);
+
+}
+
+void hookihooki::moveRight() {
+    setPos(x() - 5, y());
+}
+
+void hookihooki::moveLeft() {
+    setPos(x() + 5, y());
+}
+
+void hookihooki::runRight(){
+    if(place == 500){
+        Right->stop();
+        Left->start();
+    }
+    else{
+        place+=5;
+        setPos(x() + 5, y());
+    }
+}
+
+void hookihooki::runLeft(){
+    if(place == 0){
+        Right->start();
+        Left->stop();
+    }
+    else{
+        place-=5;
+        setPos(x() - 5, y());
+    }
+}
+
+void hookihooki::Reset(int x){
+    place = 0;
+    setPos(x, Y);
+
+}
--- src/view/hookihooki.h
+++ src/view/hookihooki.h
@@ -1,11 +1,40 @@
+#ifndef ARMIN_DOPINGI_HOOKIHOOKI_H
+#define ARMIN_DOPINGI_HOOKIHOOKI_H
 
-#ifndef GIT_AMIN_HOOKIHOOKI_H
-#define GIT_AMIN_HOOKIHOOKI_H
+#include <QGraphicsPixmapItem>
+#include <QTimer>
+#include <QSequentialAnimationGroup>
 
+class hookihooki : public QObject,public QGraphicsPixmapItem{
+Q_OBJECT
 
-class hookihooki {
+    Q_PROPERTY(qreal place READ x WRITE setX)
+public:
 
+    int current_x{};
+    hookihooki(int x, int y);
+    QPixmap * h;
+    void moveRight();
+    void moveLeft();
+
+    QTimer *Right;
+    QTimer *Left;
+
+    int place{0};
+
+    int X;
+    int Y;
+
+    bool dead{false};
+
+    void Reset(int);
+
+public slots:
+    void runRight();
+    void runLeft();
+private:
 };
+
 
 
 #endif
