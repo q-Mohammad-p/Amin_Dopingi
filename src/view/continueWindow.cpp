45dc94844dacc711f0e844853e531fa182aaaf1d
Mohammad <166171987+q-Mohammad-p@users.noreply.github.com>
2024-07-08 17:09:13 +0330

    continueWindow files added

---
+++ src/view/continueWindow.cpp
@@ -0,0 +1,2 @@
+
+#include "continueWindow.h"
---
+++ src/view/continueWindow.h
@@ -0,0 +1,16 @@
+
+
+#ifndef GIT_AMIN_CONTINUEWINDOW_H
+#define GIT_AMIN_CONTINUEWINDOW_H
+
+#include <QWidget>
+#include <QMessageBox>
+#include <QApplication>
+#include "Player.h"
+
+class ContinueWindow {
+
+};
+
+
+#endif
