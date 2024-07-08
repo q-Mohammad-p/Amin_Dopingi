d10586953d878a09411b8ac812f3bb977d36e7e0
sir-asin <ybnyady0@gmail.com>
2024-07-08 19:20:15 +0330

    controller added

--- src/view/controller.cpp
+++ src/view/controller.cpp
@@ -1,5 +1,32 @@
-//
-// Created by mobile kavosh on 7/8/2024.
-//
 
 #include "controller.h"
+
+controller::controller(Player *P) : p(P) {
+    setFlags(GraphicsItemFlag::ItemIsFocusable);
+    setFocus();
+}
+
+void controller::keyPressEvent(QKeyEvent *event) {
+    QGraphicsItem::keyPressEvent(event);
+    if (event->key() == Qt::Key_D or event->key() == Qt::Key_Right) {
+        p->Run_Right();
+    } else if (event->key() == Qt::Key_A or event->key() == Qt::Key_Left) {
+        p->Run_Left();
+    } else if (event->key() == Qt::Key_W or event->key() == Qt::Key_Up) {
+        p->Jump();
+    }
+    if (event->key() == Qt::Key_Escape)
+        exit(0);
+}
+
+void controller::keyReleaseEvent(QKeyEvent *event) {
+    QGraphicsItem::keyReleaseEvent(event);
+
+    if (event->key() == Qt::Key_D or event->key() == Qt::Key_Right) {
+        p->Standing_Right();
+    } else if (event->key() == Qt::Key_A or event->key() == Qt::Key_Left) {
+        p->Standing_Left();
+    } else if (event->key() == Qt::Key_W or event->key() == Qt::Key_Up) {
+        p->gravity();
+    }
+}
--- src/view/controller.h
+++ src/view/controller.h
@@ -1,13 +1,24 @@
 
+#ifndef AMIN_DOPINGI_CONTROLLER_H
+#define AMIN_DOPINGI_CONTROLLER_H
 
-#ifndef GIT_AMIN_CONTROLLER_H
-#define GIT_AMIN_CONTROLLER_H
 
 #include <QKeyEvent>
 #include "Player.h"
 
-class controller {
+class controller : public QObject,public QGraphicsPixmapItem{
+Q_OBJECT
 
+public:
+    controller(Player *P);
+
+protected:
+    void keyReleaseEvent(QKeyEvent *event) override;
+
+    void keyPressEvent(QKeyEvent *event) override;
+
+private:
+    Player *p;
 };
 
 
