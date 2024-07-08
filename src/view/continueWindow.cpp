66aec8cd9378f29719e94a458c3bf3d321963fd4
Mohammad <166171987+q-Mohammad-p@users.noreply.github.com>
2024-07-08 19:56:13 +0330

    continueWindow function is added to ContinueWindow

--- src/view/continueWindow.cpp
+++ src/view/continueWindow.cpp
@@ -1,2 +1,25 @@
-
 #include "continueWindow.h"
+
+ContinueWindow::ContinueWindow(QWidget *parent, Player *play) : QWidget(parent), player(play) {
+    connect(player, &Player::call, this, &ContinueWindow::continueWindow);
+
+    connect(player, &Player::call_Win, this, &ContinueWindow::winWindow);
+
+    connect(player, &Player::call_Game_Over, this, &ContinueWindow::gameOverWindow);
+}
+
+void ContinueWindow::continueWindow() {
+
+    QMessageBox msgBox;
+    QPixmap imagePixmap(":/images/youDied");
+    msgBox.setIconPixmap(imagePixmap);
+    msgBox.setStandardButtons(QMessageBox::Yes | QMessageBox::No);
+    int result = msgBox.exec();
+
+    if (result == QMessageBox::Yes) {
+        player->ReStart();
+    } else {
+        exit(0);
+    }
+
+}
--- src/view/continueWindow.h
+++ src/view/continueWindow.h
@@ -1,15 +1,28 @@
+#ifndef AMIN_DOPINGI_CONTINUEWINDOW_H
+#define AMIN_DOPINGI_CONTINUEWINDOW_H
 
-
-#ifndef GIT_AMIN_CONTINUEWINDOW_H
-#define GIT_AMIN_CONTINUEWINDOW_H
 
 #include <QWidget>
 #include <QMessageBox>
 #include <QApplication>
 #include "Player.h"
 
-class ContinueWindow {
+class ContinueWindow : public QWidget {
+Q_OBJECT
 
+public:
+    explicit ContinueWindow(QWidget *parent = nullptr, Player * = nullptr);
+
+public slots:
+
+    void continueWindow();
+
+    void winWindow();
+
+    void gameOverWindow();
+
+public:
+    Player *player;
 };
 
 
