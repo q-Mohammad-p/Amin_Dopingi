6328bde6291f8c24f98b5baca2b175109ebe5076
sir-asin <ybnyady0@gmail.com>
2024-07-08 20:27:33 +0330

    spriteStandingRight to player

--- src/view/player.cpp
+++ src/view/player.cpp
@@ -1,4 +1,4 @@
-#include "Player.h"
+#include "player.h"
 #include <iostream>
 #include <QApplication>
 #include <QWidget>
@@ -8,4 +8,23 @@
 Player::Player(Game *g, vector<PlatForm *> vect, vector <Decorator *> deco, flag *flg, int sceneHaight,
                int sceneWidth, vector<heart *> Heart,int w, int h,Position  pos, hookihooki *hook, capsule *caps,
                QGraphicsItem *parent) : QGraphicsPixmapItem(parent), BodyObject(w, h, pos) {
+
+    setFlags(GraphicsItemFlag::ItemIsFocusable);
+    plats = vect;
+    game = g;
+    Flag = flg;
+    Hearts = Heart;
+    decorators = deco;
+    Hooki = hook;
+    cap = caps;
+
+    QPixmap pixmap1(":/images/spriteStandRight");
+    pixmap1 = pixmap1.scaled(4020, 150, Qt::KeepAspectRatioByExpanding);
+    scaledImage = new QPixmap(pixmap1);
+    pixmap1 = pixmap1.copy(0, 0, 60, 150);
+    setPixmap(pixmap1);
+
+    height_ = sceneHaight - pixmap1.height() - 125;
+
+    setPos(place, height_);
 }
--- src/view/player.h
+++ src/view/player.h
@@ -1,6 +1,6 @@
 
-#ifndef GIT_AMIN_PLAYER_H
-#define GIT_AMIN_PLAYER_H
+#ifndef AMIN_DOPINGI_PLAYER_H
+#define AMIN_DOPINGI_PLAYER_H
 
 
 #include <QGraphicsPixmapItem>
@@ -28,7 +28,45 @@ public:
 
     Player(Game *, vector<PlatForm *>, vector<Decorator *>, flag *, int , int ,
            vector<heart *>,int , int ,Position  ,hookihooki *, capsule *, QGraphicsItem *parent = nullptr);
+    ~Player();
 
+    int end{100};
+    Game *game;
+    bool colliding{false};
+    bool jumped{false};
+    flag *Flag;
+    vector<PlatForm *> plats;
+    vector<heart *> Hearts;
+    vector<Decorator *> decorators;
+
+    int life{2};
+
+    hookihooki * Hooki;
+    capsule *cap;
+    bool caps{false};
+
+
+private:
+    QTimer *standRightTimer;
+    QTimer *standLeftTimer;
+    QTimer *moveRightTimer;
+    QTimer *moveLeftTimer;
+    QTimer *checkHookihookiTimer;
+    QTimer *checkCapsuleTimer;
+    int current_x = 0;
+    int current_x2 = 0;
+    int current_x3 = 0;
+    int current_x4 = 0;
+    QPixmap *scaledImage;
+    QPixmap *scaledImage2;
+    QPixmap *scaledImage3;
+    QPixmap *scaledImage4;
+    QPropertyAnimation *gravityAnimation;
+    QPropertyAnimation *jumpAnimation;
+
+    int place = 100;
+
+    int height_;
 };
 
 
