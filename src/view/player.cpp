7963169a1ccb29addcb2c6d512e806c197fc7c62
sir-asin <ybnyady0@gmail.com>
2024-07-08 20:22:32 +0330

    Player constructor added to player

--- src/view/player.cpp
+++ src/view/player.cpp
@@ -1,3 +1,11 @@
+#include "Player.h"
+#include <iostream>
+#include <QApplication>
+#include <QWidget>
+#include <QDebug>
 
 
-#include "player.h"
+Player::Player(Game *g, vector<PlatForm *> vect, vector <Decorator *> deco, flag *flg, int sceneHaight,
+               int sceneWidth, vector<heart *> Heart,int w, int h,Position  pos, hookihooki *hook, capsule *caps,
+               QGraphicsItem *parent) : QGraphicsPixmapItem(parent), BodyObject(w, h, pos) {
+}
