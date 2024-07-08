178cafcd32e70a750c2d012888b9162cdd524311
sir-asin <ybnyady0@gmail.com>
2024-07-08 17:43:47 +0330

    hills added

--- src/view/hills.cpp
+++ src/view/hills.cpp
@@ -1,3 +1,50 @@
-
-
 #include "hills.h"
+#include <QTimer>
+
+
+Hills::Hills(int sceneHeight, int sceneWidth, QGraphicsItem *parent) : QGraphicsPixmapItem(parent),
+                                                                       Decorator(sceneWidth, sceneHeight) {
+    current_x = 0;
+    setFlags(GraphicsItemFlag::ItemIsFocusable);
+
+
+    QPixmap pixmap(":/images/hills");
+    pixmap = pixmap.scaled(pixmap.width(), sceneHeight, Qt::KeepAspectRatioByExpanding);
+    scaledImage1 = new QPixmap(pixmap);
+    pixmap = pixmap.copy(0, 0, pixmap.width(), pixmap.height());
+    setPixmap(pixmap);
+
+
+}
+
+void Hills::moveRight() {
+
+    current_x = (current_x + 5) % (scaledImage1->width() - width - 1000);
+
+
+    QPixmap cropped = scaledImage1->copy(current_x, 0, width, height);
+    setPixmap(cropped);
+};
+
+
+void Hills::moveLeft() {
+
+
+    if (current_x > 0)
+        current_x = (current_x - 5) % (scaledImage1->width() - width - 1000);
+
+    QPixmap cropped = scaledImage1->copy(current_x, 0, width, height);
+
+    setPixmap(cropped);
+
+}
+
+void Hills::restart(int x) {
+    current_x = x;
+    QPixmap cropped = scaledImage1->copy(current_x, 0, width, height);
+    setPixmap(cropped);
+}
+
+Hills::~Hills() {
+    delete scaledImage1;
+}
--- src/view/hills.h
+++ src/view/hills.h
@@ -1,11 +1,28 @@
+#ifndef AMIN_DOPINGI_HILLS_H
+#define AMIN_DOPINGI_HILLS_H
 
-#ifndef GIT_AMIN_HILLS_H
-#define GIT_AMIN_HILLS_H
+#include <QGraphicsPixmapItem>
+#include "decorator.h"
+
+class Hills : public QObject, public QGraphicsPixmapItem, public Decorator {
 
 
-class Hills {
+public:
+    Hills(int, int, QGraphicsItem *parent = nullptr);
+
+    ~Hills();
+
+    int current_x{0};
+    QPixmap *scaledImage1;
+
+    void restart(int);
+
+public slots:
+
+    virtual void moveRight();
+
+    virtual void moveLeft();
 
 };
 
-
-#endif //GIT_AMIN_HILLS_H
+#endif
